bfe6f4543b198c6f7654f70e0ff51c43e280974a
Will Zhao <wzhao33@ugrad1.cs.jhu.edu>
2021-09-10 00:44:58 -0400

    fixed bug in create-from-hex

--- csf_assign01/fixedpoint.c
+++ csf_assign01/fixedpoint.c
@@ -62,6 +62,8 @@ Fixedpoint fixedpoint_create_from_hex(const char *hex) {
         temp[0] = hex[index];
         
         new_fp.frac += strtoul(temp, NULL, 16);
+        printf("%llu\n", new_fp.frac);
+        printf("%d\n", count);
         count--;
       }
       else{
@@ -77,8 +79,13 @@ Fixedpoint fixedpoint_create_from_hex(const char *hex) {
       return new_fp;
     }
   }
-
-  new_fp.frac *= pow(16, count);
+  printf("%llu\n", new_fp.frac);
+  printf("%d\n", count);
+  for(int i = 0; i< count; i++){
+    new_fp.frac *= 16;
+  }
+  
+  printf("%llu\n", new_fp.frac);
   return new_fp;
   
 }
