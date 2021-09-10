6ecbf41692428b5e4d6116a385c7fd41c9b22323
Aidan Aug <aaug1@jhu.edu>
2021-09-10 14:49:34 -0400

    Added function descriptors, worked on compare, double

--- csf_assign01/fixedpoint.c
+++ csf_assign01/fixedpoint.c
@@ -99,116 +99,15 @@ uint64_t fixedpoint_frac_part(Fixedpoint val) {
   return val.frac;
 }
 
-Fixedpoint frac_overflow(Fixedpoint left, Fixedpoint right) {
-  Fixedpoint sum_frac = fixedpoint_create(0);
-
-  // If one is negative, no overflow is possible. Return sum/diff
-  uint64_t n = sizeof(uint64_t) * 8;
-
-  if (left.is_negative == 0 && right.is_negative == 1) {
-    if (left.whole > right.whole) {
-      if (left.frac > right.frac) {
-        sum_frac.frac = left.frac - right.frac;
-      } else {
-        sum_frac.frac = right.frac - left.frac;
-        sum_frac.is_negative = 1;
-      }
-    } else if (left.whole < right.whole) {
-      if (left.frac > right.frac) {
-        sum_frac.frac = left.frac - right.frac;
-      } else {
-        sum_frac.frac = right.frac - left.frac;
-        sum_frac.is_negative = 1;
-      }
-    }
-  } else if (right.is_negative == 0 && left.is_negative == 1) {
-    if (right.whole > left.whole) {
-      if (right.frac > left.frac) {
-        sum_frac.frac = right.frac - left.frac;
-      } else {
-        sum_frac.frac = left.frac - right.frac;
-        sum_frac.is_negative = 1;
-      }
-    } else if (right.whole < left.whole) {
-      if (right.frac > left.frac) {
-        sum_frac.frac = right.frac - left.frac;
-      } else {
-        sum_frac.frac = left.frac - right.frac;
-        sum_frac.is_negative = 1;
-      }
-    }
-    return sum_frac;
-  }
-
-  /*
-  if (left.is_negative != right.is_negative) {
-    if (left.frac > right.frac) {
-      sum_frac.frac = left.frac - right.frac;
-      if (left.is_negative) {
-        sum_frac.is_negative = 1;
-      }
-    } else {
-      sum_frac.frac = right.frac - left.frac;
-      if (right.is_negative) {
-        sum_frac.is_negative = 1;
-      }
-    }
-    return sum_frac;
-  }
-  */
-
-  // If both + or both -, overflow can occur
-  if (left.is_negative && right.is_negative) {
-    sum_frac.is_negative = 1;
-  }
-  
-  // If overflow occurs, the resulting magnitude will be less than both of the other magnitudes
-  uint64_t eff_sum = (left.frac + right.frac) % ((uint64_t) pow(2, 63));
-  sum_frac.frac = eff_sum; // Effective sum
-
-  uint64_t summed_frac = left.frac + right.frac;
-  if (left.frac > summed_frac && right.frac > summed_frac) {// This means overflow occurred
-    sum_frac.whole = 1;
-  }
-
-  return sum_frac;
-
-}
-
-Fixedpoint whole_overflow(Fixedpoint left, Fixedpoint right) {
-  Fixedpoint sum_whole = fixedpoint_create(0);
-
-  // If either is negative but not both, no possible overflow
-  if (left.is_negative != right.is_negative) {
-    if (left.whole > right.whole) {
-      sum_whole.whole = left.whole - right.whole;
-      if (left.is_negative) {
-        sum_whole.is_negative = 1;
-      }
-    } else {
-      sum_whole.whole = right.whole - left.whole;
-      if (right.is_negative) {
-        sum_whole.is_negative = 1;
-      }
-    }
-    return sum_whole;
-  }
-
-  // If both + or both -, have to check overflow
-  uint64_t n = sizeof(uint64_t) * 8;
-
-  if (left.is_negative && right.is_negative) {
-    sum_whole.is_negative = 1;
-  }
-
-  sum_whole.whole = left.whole + right.whole;
-  if (left.whole > sum_whole.whole && right.whole > sum_whole.whole) {// This means overflow occurred
-    sum_whole.is_overflow = 1;
-  }
-
-  return sum_whole;
-}
-
+// Computes the sum of a positive and negative number
+//
+// Parameters:
+//   pos - the positive Fixedpoint value
+//   neg - the negative Fixedpoint value
+//
+// Returns:
+//     the Fixedpoint value for the sum of a positive
+//     and negative number
 Fixedpoint add_diff_sign(Fixedpoint pos, Fixedpoint neg) {
   Fixedpoint sum = fixedpoint_create(0);
 
@@ -238,8 +137,17 @@ Fixedpoint add_diff_sign(Fixedpoint pos, Fixedpoint neg) {
   return sum;
 }
 
+// Computes the mag of two same-signed numbers
+//
+// Parameters:
+//   left - the left Fixedpoint value
+//   right - the right Fixedpoint value
+//
+// Returns:
+//     the Fixedpoint value with the magnitude of
+//     the sum for two same-signed numbers
 Fixedpoint add_same_sign(Fixedpoint left, Fixedpoint right) {
-  // If both + or both -, have to check overflow
+  // Note: If both + or both -, have to check overflow
   Fixedpoint sum = fixedpoint_create(0);
 
   uint64_t n = sizeof(uint64_t) * 8;
@@ -260,7 +168,6 @@ Fixedpoint add_same_sign(Fixedpoint left, Fixedpoint right) {
 
 Fixedpoint fixedpoint_add(Fixedpoint left, Fixedpoint right) {
   // Account for overflow, underflow, negative
-  // Are they different sign?
   Fixedpoint sum;
 
   if (left.is_negative == 0 && right.is_negative == 1) {
@@ -296,14 +203,42 @@ Fixedpoint fixedpoint_halve(Fixedpoint val) {
 
 Fixedpoint fixedpoint_double(Fixedpoint val) {
   // TODO: implement
-  assert(0);
-  return DUMMY;
+  Fixedpoint doubled = fixedpoint_add(val, val);
+  return doubled;
 }
 
 int fixedpoint_compare(Fixedpoint left, Fixedpoint right) {
   // TODO: implement
-  assert(0);
-  return 0;
+  int compare = 0;;
+  if (fixedpoint_is_zero(left) == 1 && fixedpoint_is_zero(right) == 1) {
+    return compare;
+  }
+
+  if ((left.is_negative == 0 || fixedpoint_is_zero(left)) && right.is_negative == 1) {
+    compare = 1;
+  } else if (left.is_negative == 1 && (right.is_negative == 0 || fixedpoint_is_zero(right))) {
+    compare = -1;
+  } else if (left.is_negative == right.is_negative) {
+    if (left.whole > right.whole) {
+      compare = 1;
+    } else if (left.whole < right.whole) {
+      compare = -1;
+    } else { // equal
+      if (left.frac > right.frac) {
+        compare = 1;
+      } else if (left.frac < right.frac) {
+        compare = -1;
+      } else {
+        compare = 0;
+      }
+    }
+  }
+
+  if (left.is_negative == 1 && right.is_negative == 1) {
+    compare *= -1;
+  }
+
+  return compare;
 }
 
 int fixedpoint_is_zero(Fixedpoint val) {
