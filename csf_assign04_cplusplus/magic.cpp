2acff7f5f8086a7240f37a01a8f8f602bddddc5c
Aidan Aug <aaug1@jhu.edu>
2021-11-10 15:27:16 -0500

    handles not an elf file now

--- csf_assign04_cplusplus/magic.cpp
+++ csf_assign04_cplusplus/magic.cpp
@@ -25,12 +25,6 @@ void print_summary(uint16_t objtype, uint16_t machtype, uint16_t endianness);
 void print_section_summary(unsigned char* elf_header_uc, int symbol_sec[2]);
 void print_symbol_summary(unsigned char* elf_header_uc, int sym_index, int sym_name_index);
 
-/*
-TODO:
-1. Where do we do "not an elf file"?
-2. 
-*/
-
 int main(int argc, char **argv) {
 
   // First, make sure to handle all of the argyments
@@ -46,88 +40,24 @@ int main(int argc, char **argv) {
     data = mmap(NULL, file_size, PROT_READ, MAP_PRIVATE, fd, 0);
   }
 
-  // Get all the info regarding the elf header
+  // Make sure we have an ELF file using magic numbers listed on Wikipedia
   unsigned char *elf_header_uc = static_cast<unsigned char*> (data);
-  Elf64_Ehdr *elf_header = reinterpret_cast<Elf64_Ehdr *> (elf_header_uc);
-
-  print_summary((uint16_t)elf_header->e_type, (uint16_t)elf_header->e_machine, (uint16_t)elf_header->e_ident[EI_DATA]);
-
-
-
-  // Get all of the information about sections
-  /*
-  int num_headers = elf_header->e_shnum;
-  int sym_index = -1, sym_name_index = -1;
-  for (int i = 0; i < num_headers; i++) {
-    unsigned char *sec_header_uc = sec_headers_top + (i * sec_size);
-    Elf64_Shdr *sec_header = reinterpret_cast<Elf64_Shdr *>(sec_header_uc); // pointer to section header
-    char* name = (char*) (elf_header_uc + sec_names->sh_offset + sec_header->sh_name); // gets the section name
-    printf("Section header %d: name=%s, ", i, name);
-    printf("type=%lx, ", (long unsigned int) sec_header->sh_type);
-    printf("offset=%lx, ", (long unsigned int) sec_header->sh_offset);
-    printf("size=%lx\n", (long unsigned int) sec_header->sh_size);
-
-    if (strcmp(name, ".symtab") == 0) { // Save the index of .symtab
-      sym_index = i;
-    }
-    if (strcmp(name, ".strtab") == 0) {  // Save the index of .strtab
-      sym_name_index = i;
-    }
+  if (*elf_header_uc != 0x7F || *(elf_header_uc+1) != 'E' || *(elf_header_uc+2) != 'L' || *(elf_header_uc+3) != 'F') {
+    fprintf(stdout, "Not an ELF file\n");
+    exit(0);
   }
-  */
 
+  Elf64_Ehdr *elf_header = reinterpret_cast<Elf64_Ehdr *> (elf_header_uc);
+  print_summary((uint16_t)elf_header->e_type, (uint16_t)elf_header->e_machine, (uint16_t)elf_header->e_ident[EI_DATA]);
   int sym_sec[2];
   print_section_summary(elf_header_uc, sym_sec);
   int sym_index = sym_sec[0];
   int sym_name_index = sym_sec[1];
   print_symbol_summary(elf_header_uc, sym_index, sym_name_index);
   
-
-  
-/*
-  // Iterate over all the symbols and printf the required data
-  for (int i = 0; i < num_sym; i++) {
-    unsigned char *sym_uc = (unsigned char*)(elf_header_uc + sec_symbols->sh_offset + (i * sec_symbols->sh_entsize));
-    Elf64_Sym *sym = reinterpret_cast<Elf64_Sym *>(sym_uc); // pointer to symbol headers
-    char* name = (char*) (elf_header_uc + sec_strtab->sh_offset + sym->st_name); // gets the symbol names
-    printf("Symbol %d: name=%s, ", i, name);
-    printf("size=%lx, ", (long unsigned int) sym->st_size);
-    printf("info=%lx, ", (long unsigned int) sym->st_info);
-    printf("other=%lx\n", (long unsigned int) sym->st_other);
-  }
-  */
 }
 
-void print_symbol_summary(unsigned char* elf_header_uc, int sym_index, int sym_name_index) {
-
-  // Next, get a pointer to section .symtab
-  Elf64_Ehdr *elf_header = reinterpret_cast<Elf64_Ehdr *> (elf_header_uc);
-  unsigned char *sec_headers_top = elf_header_uc + elf_header->e_shoff;
-  int sec_size = elf_header->e_shentsize;
-
-
-  unsigned char *sec_symbols_uc = sec_headers_top + (sym_index * sec_size);
-  Elf64_Shdr *sec_symbols = reinterpret_cast<Elf64_Shdr *> (sec_symbols_uc);
-  int num_sym = sec_symbols->sh_size / sec_symbols->sh_entsize;
-
-  // Also, get a pointer to section .strtab
-  unsigned char *sec_strtab_uc = sec_headers_top + (sym_name_index * sec_size);
-  Elf64_Shdr *sec_strtab = reinterpret_cast<Elf64_Shdr *> (sec_strtab_uc);
-
-
-  for (int i = 0; i < num_sym; i++) {
-    unsigned char *sym_uc = (unsigned char*)(elf_header_uc + sec_symbols->sh_offset + (i * sec_symbols->sh_entsize));
-    Elf64_Sym *sym = reinterpret_cast<Elf64_Sym *>(sym_uc); // pointer to symbol headers
-    char* name = (char*) (elf_header_uc + sec_strtab->sh_offset + sym->st_name); // gets the symbol names
-    printf("Symbol %d: name=%s, ", i, name);
-    printf("size=%lx, ", (long unsigned int) sym->st_size);
-    printf("info=%lx, ", (long unsigned int) sym->st_info);
-    printf("other=%lx\n", (long unsigned int) sym->st_other);
-  }
-}
-
-
-
+// Handle the potential inputs
 int handle_arguments(int argc, char* filename) {
   if (argc != 2) {
     cerr << "Incorrect number of arguments!" << endl;
@@ -139,9 +69,10 @@ int handle_arguments(int argc, char* filename) {
     cerr << "Could not open File" << endl;
     exit(2);
   }
-  return fd;  
+  return fd;
 }
 
+// Print the summary with the ELF file
 void print_summary(uint16_t objtype, uint16_t machtype, uint16_t endianness) {
   cout << "Object file type: " << get_type_name(objtype) << endl;
   cout << "Instruction set: " << get_machine_name(machtype) << endl;
@@ -155,6 +86,7 @@ void print_summary(uint16_t objtype, uint16_t machtype, uint16_t endianness) {
   }
 }
 
+// Prints all of the required section data
 void print_section_summary(unsigned char* elf_header_uc, int symbol_sec[2]) {
   Elf64_Ehdr *elf_header = reinterpret_cast<Elf64_Ehdr *> (elf_header_uc);
   int sec_size = elf_header->e_shentsize;
@@ -165,7 +97,6 @@ void print_section_summary(unsigned char* elf_header_uc, int symbol_sec[2]) {
   Elf64_Shdr *sec_names = reinterpret_cast<Elf64_Shdr *> (sec_names_uc);
 
   int num_headers = elf_header->e_shnum;
-  int sym_index = -1, sym_name_index = -1;
   for (int i = 0; i < num_headers; i++) {
     unsigned char *sec_header_uc = sec_headers_top + (i * sec_size);
     Elf64_Shdr *sec_header = reinterpret_cast<Elf64_Shdr *>(sec_header_uc); // pointer to section header
@@ -176,13 +107,37 @@ void print_section_summary(unsigned char* elf_header_uc, int symbol_sec[2]) {
     printf("size=%lx\n", (long unsigned int) sec_header->sh_size);
 
     if (strcmp(name, ".symtab") == 0) { // Save the index of .symtab
-      sym_index = i;
+      symbol_sec[0] = i;
     }
     if (strcmp(name, ".strtab") == 0) {  // Save the index of .strtab
-      sym_name_index = i;
+      symbol_sec[1] = i;
     }
   }
+}
 
-  symbol_sec[0] = sym_index;
-  symbol_sec[1] = sym_name_index;
+// Prints all of the required symbol data
+void print_symbol_summary(unsigned char* elf_header_uc, int sym_index, int sym_name_index) {
+  // Get a pointer to section .symtab
+  Elf64_Ehdr *elf_header = reinterpret_cast<Elf64_Ehdr *> (elf_header_uc);
+  unsigned char *sec_headers_top = elf_header_uc + elf_header->e_shoff;
+  int sec_size = elf_header->e_shentsize;
+
+  unsigned char *sec_symbols_uc = sec_headers_top + (sym_index * sec_size);
+  Elf64_Shdr *sec_symbols = reinterpret_cast<Elf64_Shdr *> (sec_symbols_uc);
+  int num_sym = sec_symbols->sh_size / sec_symbols->sh_entsize;
+
+  // Also, get a pointer to section .strtab
+  unsigned char *sec_strtab_uc = sec_headers_top + (sym_name_index * sec_size);
+  Elf64_Shdr *sec_strtab = reinterpret_cast<Elf64_Shdr *> (sec_strtab_uc);
+
+  // Print output for all symbols
+  for (int i = 0; i < num_sym; i++) {
+    unsigned char *sym_uc = (unsigned char*)(elf_header_uc + sec_symbols->sh_offset + (i * sec_symbols->sh_entsize));
+    Elf64_Sym *sym = reinterpret_cast<Elf64_Sym *>(sym_uc); // pointer to symbol headers
+    char* name = (char*) (elf_header_uc + sec_strtab->sh_offset + sym->st_name); // gets the symbol names
+    printf("Symbol %d: name=%s, ", i, name);
+    printf("size=%lx, ", (long unsigned int) sym->st_size);
+    printf("info=%lx, ", (long unsigned int) sym->st_info);
+    printf("other=%lx\n", (long unsigned int) sym->st_other);
+  }
 }
